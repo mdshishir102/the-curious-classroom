7b8231c8234b1b1db442257070801b820b0f29f3
MD. Mahfuz Shaharia Shishir <md.mahfuzshahariashishir@MDs-MacBook-Air.local>
2026-09-19 01:55:42 +0600

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -14,62 +15,16 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "The Curious Classroom | Explore • Learn • Understand",
-
   description:
     "The Curious Classroom explains science, technology, history and fascinating ideas through simple and engaging content.",
-
-  keywords: [
-    "The Curious Classroom",
-    "Science",
-    "Technology",
-    "History",
-    "Education",
-    "Online Learning",
-  ],
-
   verification: {
     google: "ZhbYuxL7znnBk3uGPO5aagRt-LcQ2FbqrNiljZuyK80",
   },
-
-  icons: {
-    icon: "/favicon.png",
-  },
-
   openGraph: {
     title: "The Curious Classroom | Explore • Learn • Understand",
-
     description:
       "Explore science, technology, history and fascinating ideas through simple and engaging content.",
-
-    url: "https://thecuriousclassroom.vercel.app",
-
-    siteName: "The Curious Classroom",
-
-    images: [
-      {
-        url: "https://thecuriousclassroom.vercel.app/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "The Curious Classroom",
-      },
-    ],
-
-    locale: "en_US",
-
-    type: "website",
-  },
-
-  twitter: {
-    card: "summary_large_image",
-
-    title: "The Curious Classroom | Explore • Learn • Understand",
-
-    description:
-      "Explore science, technology, history and fascinating ideas through simple and engaging content.",
-
-    images: [
-      "https://thecuriousclassroom.vercel.app/og-image.png",
-    ],
+    images: ["/og-image.png"],
   },
 };
 
@@ -86,6 +41,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
       </body>
+
+      <GoogleAnalytics gaId="G-X51ZCDCKNZ" />
     </html>
   );
 }
